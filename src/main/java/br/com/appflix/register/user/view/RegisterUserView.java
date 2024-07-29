499bb8fc8f685409d345678d478a72ab9a0beb80
Wesley Werikis <wesleywerikis@outlook.com>
2024-07-29 17:33:49 -0300

    feat [view] : Created method enableTextFieldDataUser and too created local variable (name, username, password, confirmPassword and email) in the action btnUserRegister_Save

--- src/main/java/br/com/appflix/register/user/view/RegisterUserView.java
+++ src/main/java/br/com/appflix/register/user/view/RegisterUserView.java
@@ -6,10 +6,13 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.text.NumberFormat;
+
 import javax.swing.SwingConstants;
 import javax.swing.JTextField;
 import java.awt.Color;
 import javax.swing.border.MatteBorder;
+import javax.swing.text.NumberFormatter;
 import javax.swing.UIManager;
 import javax.swing.JPasswordField;
 import javax.swing.JFormattedTextField;
@@ -17,6 +20,8 @@ import javax.swing.JButton;
 import javax.swing.border.LineBorder;
 import java.awt.Cursor;
 import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class RegisterUserView {
 
@@ -81,8 +86,10 @@ public class RegisterUserView {
 		panelRegisterUser_Buttons.setBounds(10, 11, 471, 63);
 		panelRegisterUser.add(panelRegisterUser_Buttons);
 		panelRegisterUser_Buttons.setLayout(null);
-		
+			
 		JFormattedTextField formattedTextFieldUserRegister_Search = new JFormattedTextField();
+		formattedTextFieldUserRegister_Search.setForeground(new Color(57, 62, 70));
+		formattedTextFieldUserRegister_Search.setFont(new Font("Tahoma", Font.BOLD, 12));
 		formattedTextFieldUserRegister_Search.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(215, 215, 215)));
 		formattedTextFieldUserRegister_Search.setBackground(new Color(240, 240, 240));
 		formattedTextFieldUserRegister_Search.setHorizontalAlignment(SwingConstants.CENTER);
@@ -90,6 +97,12 @@ public class RegisterUserView {
 		panelRegisterUser_Buttons.add(formattedTextFieldUserRegister_Search);
 		
 		JButton btnUserRegister_Add = new JButton("");
+		btnUserRegister_Add.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				formattedTextFieldUserRegister_Search.setEnabled(false);
+				enableTextFieldDataUser();
+			}
+		});
 		btnUserRegister_Add.setIcon(new ImageIcon(RegisterUserView.class.getResource("/resources/img/button/add.png")));
 		btnUserRegister_Add.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btnUserRegister_Add.setBorder(new LineBorder(new Color(240, 240, 240)));
@@ -130,6 +143,19 @@ public class RegisterUserView {
 		panelRegisterUser_Buttons.add(btnUserRegister_Delete);
 		
 		btnUserRegister_Save = new JButton("");
+		btnUserRegister_Save.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				String name = textFieldUserRegister_Name.getText();
+				String username = textFieldUserRegister_Username.getText();
+				String password = new String(passwordFieldUserRegister_Password.getPassword());
+				String confirmPassword = new String(passwordFieldUserRegister_ConfirmPassword.getPassword());
+				String email = textFieldUserRegister_Email.getText();
+				
+				System.out.println("Dados do Usuário: " + name + " | " + username + " | " + password + " | " + confirmPassword + " | " + email);
+				
+			}
+		});
 		btnUserRegister_Save.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btnUserRegister_Save.setIcon(new ImageIcon(RegisterUserView.class.getResource("/resources/img/button/save.png")));
 		btnUserRegister_Save.setBorder(new LineBorder(new Color(240, 240, 240)));
@@ -152,6 +178,7 @@ public class RegisterUserView {
 		lblUserRegister_Name.setFont(new Font("Trebuchet MS", Font.PLAIN, 13));
 		
 		textFieldUserRegister_Name = new JTextField();
+		textFieldUserRegister_Name.setEnabled(false);
 		textFieldUserRegister_Name.setBounds(76, 22, 187, 20);
 		panelRegisterUser_DataUser.add(textFieldUserRegister_Name);
 		textFieldUserRegister_Name.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 12));
@@ -168,6 +195,7 @@ public class RegisterUserView {
 		lblUserRegister_Username.setFont(new Font("Trebuchet MS", Font.PLAIN, 13));
 		
 		textFieldUserRegister_Username = new JTextField();
+		textFieldUserRegister_Username.setEnabled(false);
 		textFieldUserRegister_Username.setBounds(350, 22, 187, 20);
 		panelRegisterUser_DataUser.add(textFieldUserRegister_Username);
 		textFieldUserRegister_Username.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 12));
@@ -184,6 +212,7 @@ public class RegisterUserView {
 		panelRegisterUser_DataUser.add(lblUserRegister_Password);
 		
 		passwordFieldUserRegister_Password = new JPasswordField();
+		passwordFieldUserRegister_Password.setEnabled(false);
 		passwordFieldUserRegister_Password.setBorder(new MatteBorder(0, 0, 1, 0, (Color) new Color(192, 192, 192)));
 		passwordFieldUserRegister_Password.setBackground(new Color(240, 240, 240));
 		passwordFieldUserRegister_Password.setBounds(76, 68, 155, 20);
@@ -198,6 +227,7 @@ public class RegisterUserView {
 		panelRegisterUser_DataUser.add(lblUserRegister_ConfirmPassword);
 		
 		passwordFieldUserRegister_ConfirmPassword = new JPasswordField();
+		passwordFieldUserRegister_ConfirmPassword.setEnabled(false);
 		passwordFieldUserRegister_ConfirmPassword.setBorder(new MatteBorder(0, 0, 1, 0, (Color) new Color(192, 192, 192)));
 		passwordFieldUserRegister_ConfirmPassword.setBackground(new Color(240, 240, 240));
 		passwordFieldUserRegister_ConfirmPassword.setBounds(382, 68, 155, 20);
@@ -212,6 +242,7 @@ public class RegisterUserView {
 		panelRegisterUser_DataUser.add(lblUserRegister_Email);
 		
 		textFieldUserRegister_Email = new JTextField();
+		textFieldUserRegister_Email.setEnabled(false);
 		textFieldUserRegister_Email.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 12));
 		textFieldUserRegister_Email.setColumns(10);
 		textFieldUserRegister_Email.setBorder(new MatteBorder(0, 0, 1, 0, (Color) new Color(192, 192, 192)));
@@ -219,4 +250,17 @@ public class RegisterUserView {
 		textFieldUserRegister_Email.setBounds(76, 116, 235, 20);
 		panelRegisterUser_DataUser.add(textFieldUserRegister_Email);
 	}
+
+	protected void enableTextFieldDataUser() {
+		
+		textFieldUserRegister_Name.setEnabled(true);
+		textFieldUserRegister_Username.setEnabled(true);
+		passwordFieldUserRegister_Password.setEnabled(true);
+		passwordFieldUserRegister_ConfirmPassword.setEnabled(true);
+		textFieldUserRegister_Email.setEnabled(true);
+		
+		textFieldUserRegister_Name.requestFocus();
+	}
+	
+	
 }
