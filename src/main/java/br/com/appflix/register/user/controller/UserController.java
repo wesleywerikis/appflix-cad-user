02ea7f1bd5c8390f3436ce1ee76ea22544a17623
Wesley Werikis <wesleywerikis@outlook.com>
2024-07-29 17:42:55 -0300

    feat [controller] : Implemented the method addUser

---
+++ src/main/java/br/com/appflix/register/user/controller/UserController.java
@@ -0,0 +1,23 @@
+package br.com.appflix.register.user.controller;
+
+import java.sql.SQLException;
+
+import br.com.appflix.register.user.dao.UserDao;
+import br.com.appflix.register.user.entity.User;
+
+public class UserController {
+	
+	private UserDao userDao;
+	
+	public UserController() {
+		this.userDao = new UserDao();
+	}
+	
+	public void addUser(User user) {
+		try {
+			userDao.insertUser(user);	
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
