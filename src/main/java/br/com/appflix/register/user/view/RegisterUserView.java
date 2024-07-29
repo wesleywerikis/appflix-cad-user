2c99ba82a12daa5df53f5b604d2162482e6b4a32
Wesley Werikis <wesleywerikis@outlook.com>
2024-07-29 11:32:54 -0300

    feat [view] : Added panel, formatted and buttons (add, left, right, edit and delete) in the view

--- src/main/java/br/com/appflix/register/user/view/RegisterUserView.java
+++ src/main/java/br/com/appflix/register/user/view/RegisterUserView.java
@@ -12,6 +12,11 @@ import java.awt.Color;
 import javax.swing.border.MatteBorder;
 import javax.swing.UIManager;
 import javax.swing.JPasswordField;
+import javax.swing.JFormattedTextField;
+import javax.swing.JButton;
+import javax.swing.border.LineBorder;
+import java.awt.Cursor;
+import javax.swing.ImageIcon;
 
 public class RegisterUserView {
 
@@ -26,6 +31,10 @@ public class RegisterUserView {
 	private JPasswordField passwordFieldUserRegister_Password;
 	private JLabel lblUserRegister_ConfirmPassword;
 	private JPasswordField passwordFieldUserRegister_ConfirmPassword;
+	private JButton btnUserRegister_Left;
+	private JButton btnUserRegister_Right;
+	private JButton btnUserRegister_Edit;
+	private JButton btnUserRegister_Delete;
 
 	/**
 	 * Launch the application.
@@ -61,9 +70,63 @@ public class RegisterUserView {
 		frameUserRegister.getContentPane().setLayout(null);
 		
 		JPanel panelRegisterUser = new JPanel();
+		panelRegisterUser.setBackground(new Color(240, 240, 240));
 		panelRegisterUser.setBounds(0, 0, 574, 259);
 		frameUserRegister.getContentPane().add(panelRegisterUser);
 		panelRegisterUser.setLayout(null);
+		
+		JPanel panelRegisterUser_Buttons = new JPanel();
+		panelRegisterUser_Buttons.setBackground(new Color(240, 240, 240));
+		panelRegisterUser_Buttons.setBounds(10, 11, 420, 63);
+		panelRegisterUser.add(panelRegisterUser_Buttons);
+		panelRegisterUser_Buttons.setLayout(null);
+		
+		JFormattedTextField formattedTextFieldUserRegister_Search = new JFormattedTextField();
+		formattedTextFieldUserRegister_Search.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(215, 215, 215)));
+		formattedTextFieldUserRegister_Search.setBackground(new Color(240, 240, 240));
+		formattedTextFieldUserRegister_Search.setHorizontalAlignment(SwingConstants.CENTER);
+		formattedTextFieldUserRegister_Search.setBounds(13, 11, 47, 41);
+		panelRegisterUser_Buttons.add(formattedTextFieldUserRegister_Search);
+		
+		JButton btnUserRegister_Add = new JButton("");
+		btnUserRegister_Add.setIcon(new ImageIcon(RegisterUserView.class.getResource("/resources/img/button/add.png")));
+		btnUserRegister_Add.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnUserRegister_Add.setBorder(new LineBorder(new Color(240, 240, 240)));
+		btnUserRegister_Add.setBackground(new Color(240, 240, 240));
+		btnUserRegister_Add.setBounds(85, 11, 47, 41);
+		panelRegisterUser_Buttons.add(btnUserRegister_Add);
+		
+		btnUserRegister_Left = new JButton("");
+		btnUserRegister_Left.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnUserRegister_Left.setIcon(new ImageIcon(RegisterUserView.class.getResource("/resources/img/button/left.png")));
+		btnUserRegister_Left.setBorder(new LineBorder(new Color(240, 240, 240)));
+		btnUserRegister_Left.setBackground(new Color(240, 240, 240));
+		btnUserRegister_Left.setBounds(151, 11, 47, 41);
+		panelRegisterUser_Buttons.add(btnUserRegister_Left);
+		
+		btnUserRegister_Right = new JButton("");
+		btnUserRegister_Right.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnUserRegister_Right.setIcon(new ImageIcon(RegisterUserView.class.getResource("/resources/img/button/right.png")));
+		btnUserRegister_Right.setBorder(new LineBorder(new Color(240, 240, 240)));
+		btnUserRegister_Right.setBackground(new Color(240, 240, 240));
+		btnUserRegister_Right.setBounds(217, 11, 47, 41);
+		panelRegisterUser_Buttons.add(btnUserRegister_Right);
+		
+		btnUserRegister_Edit = new JButton("");
+		btnUserRegister_Edit.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnUserRegister_Edit.setIcon(new ImageIcon(RegisterUserView.class.getResource("/resources/img/button/edit.png")));
+		btnUserRegister_Edit.setBorder(new LineBorder(new Color(240, 240, 240)));
+		btnUserRegister_Edit.setBackground(new Color(240, 240, 240));
+		btnUserRegister_Edit.setBounds(283, 11, 47, 41);
+		panelRegisterUser_Buttons.add(btnUserRegister_Edit);
+		
+		btnUserRegister_Delete = new JButton("");
+		btnUserRegister_Delete.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnUserRegister_Delete.setIcon(new ImageIcon(RegisterUserView.class.getResource("/resources/img/button/delete.png")));
+		btnUserRegister_Delete.setBorder(new LineBorder(new Color(240, 240, 240)));
+		btnUserRegister_Delete.setBackground(new Color(240, 240, 240));
+		btnUserRegister_Delete.setBounds(349, 11, 47, 41);
+		panelRegisterUser_Buttons.add(btnUserRegister_Delete);
 		
 		panelRegisterUser_DataUser = new JPanel();
 		panelRegisterUser_DataUser.setBackground(new Color(240, 240, 240));
@@ -72,6 +135,7 @@ public class RegisterUserView {
 		panelRegisterUser_DataUser.setLayout(null);
 		
 		JLabel lblUserRegister_Name = new JLabel("Nome:");
+		lblUserRegister_Name.setBackground(new Color(240, 240, 240));
 		lblUserRegister_Name.setBounds(13, 25, 50, 14);
 		panelRegisterUser_DataUser.add(lblUserRegister_Name);
 		lblUserRegister_Name.setHorizontalTextPosition(SwingConstants.CENTER);
@@ -87,6 +151,7 @@ public class RegisterUserView {
 		textFieldUserRegister_Name.setColumns(10);
 		
 		lblUserRegister_Username = new JLabel("Usuário:");
+		lblUserRegister_Username.setBackground(new Color(240, 240, 240));
 		lblUserRegister_Username.setBounds(276, 25, 61, 14);
 		panelRegisterUser_DataUser.add(lblUserRegister_Username);
 		lblUserRegister_Username.setHorizontalTextPosition(SwingConstants.CENTER);
@@ -99,9 +164,10 @@ public class RegisterUserView {
 		textFieldUserRegister_Username.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 12));
 		textFieldUserRegister_Username.setColumns(10);
 		textFieldUserRegister_Username.setBorder(new MatteBorder(0, 0, 1, 0, (Color) new Color(192, 192, 192)));
-		textFieldUserRegister_Username.setBackground(UIManager.getColor("Button.background"));
+		textFieldUserRegister_Username.setBackground(new Color(240, 240, 240));
 		
 		lblUserRegister_Password = new JLabel("Senha:");
+		lblUserRegister_Password.setBackground(new Color(240, 240, 240));
 		lblUserRegister_Password.setHorizontalTextPosition(SwingConstants.CENTER);
 		lblUserRegister_Password.setHorizontalAlignment(SwingConstants.CENTER);
 		lblUserRegister_Password.setFont(new Font("Trebuchet MS", Font.PLAIN, 13));
@@ -115,6 +181,7 @@ public class RegisterUserView {
 		panelRegisterUser_DataUser.add(passwordFieldUserRegister_Password);
 		
 		lblUserRegister_ConfirmPassword = new JLabel("Confirme a Senha:");
+		lblUserRegister_ConfirmPassword.setBackground(new Color(240, 240, 240));
 		lblUserRegister_ConfirmPassword.setHorizontalTextPosition(SwingConstants.CENTER);
 		lblUserRegister_ConfirmPassword.setHorizontalAlignment(SwingConstants.CENTER);
 		lblUserRegister_ConfirmPassword.setFont(new Font("Trebuchet MS", Font.PLAIN, 13));
@@ -123,11 +190,12 @@ public class RegisterUserView {
 		
 		passwordFieldUserRegister_ConfirmPassword = new JPasswordField();
 		passwordFieldUserRegister_ConfirmPassword.setBorder(new MatteBorder(0, 0, 1, 0, (Color) new Color(192, 192, 192)));
-		passwordFieldUserRegister_ConfirmPassword.setBackground(UIManager.getColor("Button.background"));
+		passwordFieldUserRegister_ConfirmPassword.setBackground(new Color(240, 240, 240));
 		passwordFieldUserRegister_ConfirmPassword.setBounds(382, 68, 155, 20);
 		panelRegisterUser_DataUser.add(passwordFieldUserRegister_ConfirmPassword);
 		
 		lblUserRegister_Email = new JLabel("E-mail:");
+		lblUserRegister_Email.setBackground(new Color(240, 240, 240));
 		lblUserRegister_Email.setHorizontalTextPosition(SwingConstants.CENTER);
 		lblUserRegister_Email.setHorizontalAlignment(SwingConstants.CENTER);
 		lblUserRegister_Email.setFont(new Font("Trebuchet MS", Font.PLAIN, 13));
@@ -138,7 +206,7 @@ public class RegisterUserView {
 		textFieldUserRegister_Email.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 12));
 		textFieldUserRegister_Email.setColumns(10);
 		textFieldUserRegister_Email.setBorder(new MatteBorder(0, 0, 1, 0, (Color) new Color(192, 192, 192)));
-		textFieldUserRegister_Email.setBackground(UIManager.getColor("Button.background"));
+		textFieldUserRegister_Email.setBackground(new Color(240, 240, 240));
 		textFieldUserRegister_Email.setBounds(76, 116, 235, 20);
 		panelRegisterUser_DataUser.add(textFieldUserRegister_Email);
 	}
