9068fdf0741f61441d4597a94babf6ed557d1fd9
Wesley Werikis <wesleywerikis@outlook.com>
2024-07-30 11:10:30 -0300

    feat [controller] : Implemented the method getUserById

--- src/main/java/br/com/appflix/register/user/controller/UserController.java
+++ src/main/java/br/com/appflix/register/user/controller/UserController.java
@@ -6,18 +6,26 @@ import br.com.appflix.register.user.dao.UserDao;
 import br.com.appflix.register.user.entity.User;
 
 public class UserController {
-	
+
 	private UserDao userDao;
-	
+
 	public UserController() {
 		this.userDao = new UserDao();
 	}
-	
+
 	public void addUser(User user) {
 		try {
-			userDao.insertUser(user);	
+			userDao.insertUser(user);
 		} catch (SQLException e) {
 			e.printStackTrace();
+		}
+	}
+
+	public User getUserById(Long id) {
+		try {
+			return UserDao.getUserById(id);
+		} catch (NumberFormatException e) {
+			return null;
 		}
 	}
 }
